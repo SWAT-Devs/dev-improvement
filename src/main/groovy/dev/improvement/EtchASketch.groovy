a82a7333214dbe4502d17cbea98522d4ddda6441
Nate Jones <n8jones@gmail.com>
2018-09-27 15:10:07 -0400

    Adding my implementation of EtchASketch

--- src/main/groovy/dev/improvement/EtchASketch.groovy
+++ src/main/groovy/dev/improvement/EtchASketch.groovy
@@ -10,7 +10,11 @@ class EtchASketch {
 	 * Print the screen to out, using 'X' for true and ' ' for false.
 	 */
 	void print(Appendable out){
-		//TODO Implement here
+		def ln = System.lineSeparator()
+		screen.each {line -> 
+			line.each {cell -> out.append(cell ? 'X' : ' ')}
+			out.append(ln)
+		}
 	}
 
 	/**
@@ -31,7 +35,60 @@ class EtchASketch {
      * See test case for examples.
 	 */
 	void run(Reader reader){
-		//TODO Implement here
+		int x = 0
+		int y = 0
+		screen = [[true]]
+		for(int i; (i = reader.read()) > 0;){
+			def c = i as char
+			List<Boolean> line = null;
+			switch(c){
+				case '>':
+					x++
+					if(x < screen[y].size()){
+						screen[y][x] = true
+					}
+					else{
+						screen[y] << true
+					}
+					break;
+				case 'v':
+					y++
+					if(y < screen.size()){
+						line = screen[y]
+					}
+					else{ 
+						line = []
+						screen << line
+					}
+					fillLeft(line, x)
+					break;
+				case '<':
+					x--
+					if(x < 0){
+						System.err.println "Too many '<'"
+						x = 0
+					}
+					screen[y][x] = true
+					break;
+				case '^':
+					y--
+					if(y < 0){
+						System.err.println "Too many '^'"
+						y = 0
+					}
+					fillLeft(screen[y], x)
+					break;
+				default:
+					System.err.println "Unrecognized symbol '$c'"
+			}
+		}
+	}
+
+	private static void fillLeft(List<Boolean> line, int x){
+		while(x >= line.size()){
+			line << false
+		}
+		line[x] = true
 	}
 
 	/**
