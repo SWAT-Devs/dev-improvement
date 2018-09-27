acc97c0f348d38c36532151231e4d17f50baff3e
Nate Jones <n8jones@gmail.com>
2018-09-27 15:09:23 -0400

    Adding new line to the end of print output.  Adding a secret message.

--- src/test/groovy/dev/improvement/EtchASketchTest.groovy
+++ src/test/groovy/dev/improvement/EtchASketchTest.groovy
@@ -13,7 +13,7 @@ class EtchASketchTest {
 		def ls = System.lineSeparator()
 		def sb = new StringBuilder()
 		eas.print(sb)
-		Assert.assertEquals("2 x 2", "X $ls X", sb.toString())
+		Assert.assertEquals("2 x 2", "X $ls X$ls".toString(), sb.toString())
 	}
 
 	@Test
@@ -28,6 +28,15 @@ class EtchASketchTest {
 		assertRunEquals(">>vv<<^^", [[true, true,  true],
 									 [true, false, true],
 									 [true, true,  true]])
+
+		// Secret Message
+		eas.run("vvvv>^^^^vvvvv>v>^>v^>^^^^^>vvvv<v<vv<<<<>>>>>>>>>>"
+				+"^^^^^^^>>>>v>>vvvv<<v<<<^^^^^vvvvvv>>>>>>>"
+		        +"^>^^^^^^<>>><vvvvvv><<v>>>>"
+		        +"^^^^^^^>>>>v>>vvvv<<v<<<^^^^^vvvvvv>>>>>>>>>"
+		        +"^>^^^^^^<>>><vvvvvv><<v>>>>"
+		        +">>>^^^^^^^<<<>>>>>>><<<vvvvvvv>>>")
+		eas.print()
 	}
 
 	private void assertRunEquals(String instructions, List<List<Boolean>> expected){
