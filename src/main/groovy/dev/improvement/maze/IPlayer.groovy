4cda1e39a06b95c9e01c87ed4570c338255815f3
Nate Jones <n8jones@gmail.com>
2018-10-19 15:50:13 -0400

    Adding some comments to make the maze code easier to understand.

--- src/main/groovy/dev/improvement/maze/IPlayer.groovy
+++ src/main/groovy/dev/improvement/maze/IPlayer.groovy
@@ -1,8 +1,26 @@
 package dev.improvement.maze
 
+/**
+ * Represents a player trying to find a route through the maze.
+ */
 interface IPlayer {
+	/**
+     * All the moves that the player has made to this point.
+     */
 	Iterable<Direction> getMovesTaken()
+
+	/**
+     * Can the player move in the given direction from their current location.
+     */
 	boolean canMove(Direction direction)
+
+	/**
+     * Move the player in the given direction.
+     */
 	void move(Direction direction)
+
+	/**
+     * Is the player standing on the finish cell.
+     */
 	boolean hasWon()
 }
--- src/main/groovy/dev/improvement/maze/MazeReader.groovy
+++ src/main/groovy/dev/improvement/maze/MazeReader.groovy
@@ -1,6 +1,20 @@
 package dev.improvement.maze
 
+/**
+ * Reads in a maze from lines of text.
+ * - lines represent the vertical axis of the maze with the top of the file 
+ *   being north and the bottom being south.
+ * - characters in the line represent the horizontal axis with left being West 
+ *   and right being East.
+ * - '#' represents walls
+ * - 'O' represents the starting position
+ * - 'X' represents the finish position
+ * - anything else is seen as an open cell
+ */
 class MazeReader {
+	/**
+     * Create a maze from the input.
+     */
 	IMaze read(Reader reader){
 		throw new IllegalStateException("not implemented")
 	}
