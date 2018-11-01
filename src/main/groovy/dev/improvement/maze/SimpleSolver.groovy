3830db916eb4932856d580fee412109243a0c6c3
Nate Jones <n8jones@gmail.com>
2018-11-01 13:26:24 -0400

    Making left turn solver slightly more efficient and easy to read.

--- src/main/groovy/dev/improvement/maze/SimpleSolver.groovy
+++ src/main/groovy/dev/improvement/maze/SimpleSolver.groovy
@@ -5,45 +5,45 @@ import static dev.improvement.maze.Direction.*
 class SimpleSolver implements ISolver {
 		@Override
 		void solve(IPlayer player) {
-				Direction last = Direction.North;
+				Direction last = North;
 				while(!player.hasWon()){
-						def left = getLeft(last)
+						Direction left = LEFT[last]
 						Direction current
 						if(player.canMove(left)){
+								//If you can turn left
 								current = left
 						}
 						else if(player.canMove(last)){
+								//If you can go straight
 								current = last
 						}
 						else{
-								def o = getOpposite(last)
-								for(def i = 0; i< 4 && (left == o || !player.canMove(left)); i++){
-										left = getLeft(left)
-								}
-								current = player.canMove(left) ? left : o;
+								Direction right = RIGHT[last]
+								//If you can turn right, else go back.
+								current = player.canMove(right) ? right : OPPOSITE[last]
 						}
 						player.move current
 						last = current
 				}
 		}
 
-		private static Direction getLeft(Direction d){
-				switch(d){
-						case North: return West
-						case West: return South
-						case South: return East
-						case East: return North
-						default: throw new IllegalStateException()
-				}
-		}
-
-		private static Direction getOpposite(Direction d){
-				switch(d) {
-						case North: return South
-						case South: return North
-						case East: return West
-						case West: return East
-						default: throw new IllegalStateException()
-				}
+		private static final Map<Direction, Direction> LEFT = new EnumMap<>(Direction)
+		private static final Map<Direction, Direction> RIGHT = new EnumMap<>(Direction)
+		private static final Map<Direction, Direction> OPPOSITE = new EnumMap<>(Direction)
+		static {
+				LEFT[North] = West
+				LEFT[West] = South
+				LEFT[South] = East
+				LEFT[East] = North
+				
+				RIGHT[North] = East
+				RIGHT[East] = South
+				RIGHT[South] = West
+				RIGHT[West] = North
+				
+				OPPOSITE[North] = South
+				OPPOSITE[South] = North
+				OPPOSITE[East] = West
+				OPPOSITE[West] = East
 		}
 }
