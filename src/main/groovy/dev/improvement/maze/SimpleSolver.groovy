78eda821407a27cb919caf89c6a3686c7ac4fe12
Nate Jones <n8jones@gmail.com>
2018-11-01 11:46:03 -0400

    The team's simple solver solution.

--- src/main/groovy/dev/improvement/maze/SimpleSolver.groovy
+++ src/main/groovy/dev/improvement/maze/SimpleSolver.groovy
@@ -1,7 +1,53 @@
 package dev.improvement.maze
 
 class SimpleSolver implements ISolver {
-		@Override
-		void solve(IPlayer player) {
+	Direction lastDirTaken;
+
+	@Override
+	void solve(IPlayer player) {
+		while(!player.hasWon()){
+			Direction dir = nextMoveRandom();
+			if(player.canMove(dir) 
+				&& (lastDirTaken == null || lastDirTaken != getOppositeDir(dir))){
+				player.move(dir)
+				lastDirTaken = dir
+			}
+			else{
+				if(lastDirTaken == Direction.North){
+					player.move(Direction.South)
+					lastDirTaken = Direction.South
+				}
+				else if(lastDirTaken == Direction.West){
+					player.move(Direction.East)
+					lastDirTaken = Direction.East
+				}
+				else if(lastDirTaken == Direction.South){
+					player.move(Direction.North)
+					lastDirTaken = Direction.North
+				}
+				else if(lastDirTaken == Direction.East){
+					player.move(Direction.West)
+					lastDirTaken = Direction.West
+				}
+			}
 		}
+	}
+
+	Direction nextMoveRandom(){
+		def r = (int) (Math.random() * 4)
+		return Direction.values()[r]
+	}
+
+	Direction getOppositeDir(Direction dir){
+		switch(dir){
+			case Direction.South:
+				return Direction.North
+			case Direction.West:
+				return Direction.East
+			case Direction.North:
+				return Direction.South
+			case Direction.East:
+				return Direction.West
+		}
+	}
 }
