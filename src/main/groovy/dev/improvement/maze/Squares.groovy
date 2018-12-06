e30043fc072f6a6650b548d3192e7e6d0dd7823f
Nate Jones <n8jones@gmail.com>
2018-12-06 11:50:12 -0500

    Removing unneeded check of midpoint

--- src/main/groovy/dev/improvement/maze/Squares.groovy
+++ src/main/groovy/dev/improvement/maze/Squares.groovy
@@ -16,7 +16,6 @@ class Squares implements ICommand{
 		if(dimension % 2 ==0){
 			dimension = dimension - 1
 		}
-		def div = dimension / 2
 		for(int y = 0; y < dimension; y++){
 			if(y > 0){
 				out << '\n'
@@ -24,9 +23,9 @@ class Squares implements ICommand{
 			for(int x = 0; x < dimension; x++) {
 				if((y % 2 == 0 && (x % 2 == 0
 													 ||(x >= y && x < dimension - y)
-													 ||(y > div && x < y && x >= dimension - y)))
+													 ||(x < y && x >= dimension - y)))
 					 ||(x % 2 == 0 && ((y >= x && y < dimension - x)
-														 ||(x > div && y < x && y >= dimension - x)))){
+														 ||(y < x && y >= dimension - x)))){
 					out << '#'
 				}
 				else {
