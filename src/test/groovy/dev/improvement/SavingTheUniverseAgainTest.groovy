857f9a1cc9f1105409ae9cfb8de5bc54508014e7
Nate Jones <n8jones@gmail.com>
2019-02-21 17:08:19 -0500

    Adding spock framework and Michaels saving the universe solution.

--- src/test/groovy/dev/improvement/SavingTheUniverseAgainTest.groovy
+++ src/test/groovy/dev/improvement/SavingTheUniverseAgainTest.groovy
@@ -1,23 +1,57 @@
 package dev.improvement
 
-import org.junit.*
+import spock.lang.*
 
 /**
  * From https://codejam.withgoogle.com/2018/challenges/00000000000000cb/dashboard
  */
-@groovy.transform.CompileStatic
-class SavingTheUniverseAgainTest {
-  @Test
-  void can_we_hack_it() {
-    assert howManySwaps(1, 'CS')     == 1
-    assert howManySwaps(2, 'CS')     == 0
-    assert howManySwaps(1, 'SS')     == -1 //IMPOSSIBLE
-    assert howManySwaps(6, 'SCCSSC') == 2
-    assert howManySwaps(2, 'CC')     == 0
-    assert howManySwaps(3, 'CSCSS')  == 5
+abstract class SavingTheUniverseAgain extends Specification {
+  HowMany impl
+  @Unroll
+  def "howMany (#shield, '#program') = #expected"() {
+    expect:
+      impl.howMany(shield, program) == expected
+    where:
+      shield | program                          || expected
+      1      | 'CS'                             || 1
+      2      | 'CS'                             || 0
+      1      | 'SS'                             || -1
+      6      | 'SCCSSC'                         || 2
+      2      | 'CC'                             || 0
+      3      | 'CSCSS'                          || 5
+      3      | 'CSSS'                           || 3
+      15     | 'CCCCCCCCCCCCCCCSSSSSSSSSSSSSSS' || 225
+  }
+}
+
+class SavingTheUniverseAgainMichaelsTest extends SavingTheUniverseAgain {
+  def setup() { impl = new Michaels() }
+}
+
+
+class SavingTheUniverseAgainGroupsTest extends SavingTheUniverseAgain {
+  def setup() { impl = new Groups() }
+}
+
+abstract class HowMany {
+  abstract int howMany(int d, String p)
+  
+  int strength(char[] command){
+    int power = 1;
+    int total = 0;
+    for(char c : command){
+      if(c == 'C') power =power << 1;
+      if(c == 'S') total += power;
+    }
+    return total;
   }
 
-  int howManySwaps(int d, String p) {
+  @Override String toString() { getClass().simpleName }
+}
+
+class Groups extends HowMany {
+  @Override
+  int howMany(int d, String p) {
     int swapCount = 0;
 
     char[] cmd = p.toCharArray()
@@ -40,7 +74,7 @@ class SavingTheUniverseAgainTest {
     }
     -1
   }
-  
+
   boolean swap(char[] cmd){
     int lastS = cmd.length - 1
     for(; lastS>= 0; lastS--)
@@ -57,14 +91,44 @@ class SavingTheUniverseAgainTest {
     cmd[lastS - 1] = 'S';
     return true
   }
+}
 
-  int strength(char[] command){
-    int power = 1;
-    int total = 0;
-    for(char c : command){
-      if(c == 'C') power =power << 1;
-      if(c == 'S') total += power;
+class Michaels extends HowMany {
+  @Override
+  int howMany(int shieldStr, String sCmd) {
+		char[] cmd = sCmd.toCharArray();
+		int swapCount = 0;
+		int laserPower = strength(cmd);
+		int lastShotInd = cmd.length - 1;
+		for (; lastShotInd > 0; lastShotInd--) {
+			if (cmd[lastShotInd] == 'S' && cmd[lastShotInd - 1] == 'C')
+				break;
+
+		}
+
+		int lastShotPower = 1;
+
+		for (int i = 0; i < lastShotInd; i++){
+			if (cmd[i] == 'C')
+				lastShotPower *= 2;
     }
-    return total;
-  }
+		while (laserPower > shieldStr) {
+			if (lastShotInd == 0)
+				return -1;
+
+			cmd[lastShotInd] = 'C';
+			cmd[lastShotInd - 1] = 'S';
+			laserPower -= (int)(lastShotPower / 2);
+			swapCount++;
+
+			if (lastShotInd < cmd.length - 1 && cmd[lastShotInd + 1] == 'S')
+				lastShotInd++;
+			else {
+				while (lastShotInd > 0 && cmd[lastShotInd - 1] == 'S')
+					lastShotInd--;
+				lastShotPower -= (int)(lastShotPower / 2);
+			}
+		}
+		return swapCount;
+	}
 }
