b1655762109da730a89278a0c0ec7e78a909f4ea
Nate Jones <n8jones@gmail.com>
2019-06-07 09:39:37 -0400

    Team's 3rd attempt at the maximal square problem.

--- src/test/groovy/dev/improvement/MaximalSquareTest.groovy
+++ src/test/groovy/dev/improvement/MaximalSquareTest.groovy
@@ -34,13 +34,34 @@ class MaximalSquareTest extends Specification {
     int biggestSquare = 0;
     for(int row = 0; row < strArr.size(); row++) {
       for(int col = 0; col < strArr[row].size(); col++) {
-        if(strArr[row][column] == "1") {
-          biggestSquare = Math.max(biggestSquare, findLargestSquare(row, column)
+        if(strArr[row][col] == "1") {
+          biggestSquare = Math.max(biggestSquare, findLargestSquare(row, col, strArr))
         }
       }
     }
   }
 
+  static int findLargestSquare(int row, int col, List<String> strArr) {
+    int sizeOfSquare = 1;
+    while(canFindSquareOfSize(row, col, strArr, sizeOfSquare)) {
+      sizeOfSquare++;
+    }
+    return sizeOfSquare;
+  }
+
+  static boolean canFindSquareOfSize(int row, int col, List<String> strArr, int size) {
+    if(row + size >= strArr.size() ||
+      col + size >= strArr[0].size())
+      return false;
+    for(int rowTraverse = row; rowTraverse < row + size; rowTraverse++) {
+      for(int colTraverse = col; colTraverse < col + size; colTraverse++) {
+        if(strArr[rowTraverse][colTraverse] == '0')
+          return false;
+      }
+    }
+    return true;
+  }
+/*
   static int findLargestSquare(int row, int col, List<String> strArr) {
     int maxRowDiff = 0;
     for(int c = col+1; c < strArr[row].size(); c++) {
@@ -106,4 +127,5 @@ class MaximalSquareTest extends Specification {
     }
     return size  * size 
   }
+  */
 }
